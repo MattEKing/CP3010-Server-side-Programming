c6c09ccd10b32319b643908cb544af914f1cacd3
Matthew King <146943749+MattEKing@users.noreply.github.com>
2026-01-08 10:49:43 -0330

    Exercise 3 Finished

--- Review/Exercise3/exercise1.js
+++ Review/Exercise3/exercise1.js
@@ -1,7 +1,31 @@
 "use strict";
 
 const $ = selector => document.querySelector(selector);
+let timer = null; 
+let interval = 1
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+    $("#start").addEventListener('click', () => {startTimer()});
+    $("#stop").addEventListener('click', () => {StopTimer()});
 });
+
+const startTimer = () => {
+    if (timer == null) {
+    timer = setInterval(addIterval, 1000)
+    }
+};
+
+const StopTimer = () => {
+    if (timer != null) {
+    clearInterval(timer)
+    timer = null
+    interval = 1
+    }
+}
+
+const addIterval = () => {
+    interval += 1
+    $("#count").textContent = interval;
+    console.log(interval)
+
+}
