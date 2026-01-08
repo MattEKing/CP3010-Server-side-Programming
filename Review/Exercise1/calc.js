b247093a7dbc82dda8903384822ccb36080d4191
Matthew King <146943749+MattEKing@users.noreply.github.com>
2026-01-08 10:14:56 -0330

    exercise 3

--- Review/Exercise1/calc.js
+++ Review/Exercise1/calc.js
@@ -1,6 +1,31 @@
 const $ = (selector) => document.querySelector(selector);
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    
+    $('#calculate').addEventListener("click", () => {calcTaxRate()});
+    $('#reset').addEventListener("click", () => {resetTextBox()});
 });
+
+const calcTaxRate = () => {
+    let price = parseFloat($("#price").value);
+    let rate = parseFloat($("#rate").value) / 100;
+    if (isNaN(price) || price == 0) {
+        alert("Enter valid float.")
+        $("#price").value = ""
+        $("#rate").value = ""
+    }
+    if (!isNaN(price) && price > 0 && rate >= 2 || rate <= 12) {
+        let tax = price * rate;
+        let total = price + tax;
+        $("#total").value = total;
+    }
+    else {
+        alert("Enter a rate between 2 and 12.")
+        $("#rate").value = ""
+    }
+}
+
+const resetTextBox = () => {
+    $("#price").value = ""
+    $("#rate").value = ""
+    $("#total").value = ""
+}
--- Review/Exercise2/exercise.js
+++ Review/Exercise2/exercise.js
@@ -4,5 +4,57 @@ const $ = (selector) => document.querySelector(selector);
 
 
 document.addEventListener("DOMContentLoaded", () => {
-   
+   $("#login").addEventListener("click", () =>{login()})
+   $("#resetButton").addEventListener("click", () => {reset()})
 });
+
+const login = () => {
+    let email = $("#email_address1").value;
+    let password = $("#password").value;
+    let robot = $("#robot").checked;
+
+    let isValidEmail = false;
+    let isValidPassword = false;
+
+    if (email == "") {
+        email = " ";
+        document.getElementById("email_address1").nextElementSibling.textContent = "Invalid Email";
+        isValidEmail = false;
+    }
+    else {
+        isValidEmail = true;
+        document.getElementById("email_address1").nextElementSibling.textContent = "*";
+    }
+
+    if (password == "") {
+        password = " ";
+        document.getElementById("password").nextElementSibling.textContent = "Invalid Password";
+        isValidPassword = false;
+    }
+    else {
+        isValidPassword = true;
+        document.getElementById("password").nextElementSibling.textContent = "*";
+    }
+
+    if (!robot) {
+        document.getElementById("robot").nextElementSibling.textContent = "Need to check box";
+    }
+    else {    
+        document.getElementById("robot").nextElementSibling.textContent = "*";
+    }
+
+    if (isValidEmail && isValidPassword) {
+        document.location = "join.html";
+    }
+
+}
+
+const reset = () => {
+    $("#email_address1").value = "";
+    document.getElementById("email_address1").nextElementSibling.textContent = "*";
+    $("#password").value = "";
+    document.getElementById("password").nextElementSibling.textContent = "*";
+    $("#robot").checked = false;
+    document.getElementById("robot").nextElementSibling.textContent = "*";
+
+}
