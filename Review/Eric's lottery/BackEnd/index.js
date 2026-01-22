f4a724079fe4468eb269c22a146fbc6421b0174a
Matthew King <146943749+MattEKing@users.noreply.github.com>
2026-01-22 15:22:19 -0330

    updated eric's lottery

---
+++ Review/Eric's lottery/BackEnd/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = new express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    response.json()
+})
+
+app.post("/api/checkticket", (request, response) => {
+    const body = request.body
+})
+
+
