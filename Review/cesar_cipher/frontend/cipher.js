d385b1db35ba31616d1a17279da5b7b9d3d1582f
Matthew King <146943749+MattEKing@users.noreply.github.com>
2026-01-27 09:56:18 -0330

    updated cipher

--- Review/cesar_cipher/frontend/cipher.js
+++ Review/cesar_cipher/frontend/cipher.js
@@ -2,7 +2,31 @@
 
 const getElement = (selector) => document.querySelector(selector);
 
-document.addEventListener("DOMContentLoaded", () => {
-    
+const url = "http://127.0.0.1:3000/api/encrypt"
+
+document.addEventListener("DOMContentLoaded", async() => {
+
+    const info = getElement("#info").value;
+    const shift = 5;
+
+    try {
+        const option = {
+            method:"POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({info, shift})
+        }
+        const response = await fetch(url, option);
+        const json = await response.json()
+
+        if (json.error) {
+            console.log("Server error: " + json.error.message);
+        }
+        else {
+            response = await fetch(url);
+            info.value = json;
+        }
+    } catch (e) {
+        console.log(e);
+    }
 })
 
