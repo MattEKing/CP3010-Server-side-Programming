88374c58865500d8663ca5ec5e779b31ea2b2f7a
Matthew King <146943749+MattEKing@users.noreply.github.com>
2026-02-02 20:23:25 -0330

    Finished assignment 2

--- Assignments/Assignment2/Eric's Weather/BackEnd/index.js
+++ Assignments/Assignment2/Eric's Weather/BackEnd/index.js
@@ -1,43 +1,47 @@
-"use-strict"
+"use-strict";
 
 import express from "express";
 import cors from "cors";
 
 const json = {
-    "Toronto" : {
-        "description" : 'cloudy',
-        "temperature" : ' 7°c',
-        "windSpeed": '15 km/hr'
-    },
-    "Vancouver" : {
-        "description" : 'sunny',
-        "temperature" : '25°c',
-        "windSpeed": '12 km/hr'
-    },
-    "Montreal" : {
-        "description" : 'rainy',
-        "temperature" : '10°c',
-        "windSpeed": '10 km/hr'
-    },
-    "Halifax" : {
-        "description" : 'snowy',
-        "temperature" : '-2°c',
-        "windSpeed": '30 km/hr'
-    }
-}
+  Toronto: {
+    lat: 43.65,
+    lon: -79.38,
+  },
+  Vancouver: {
+    lat: 49.28,
+    lon: -123.12,
+  },
+  Montreal: {
+    lat: 45.5,
+    lon: -73.56,
+  },
+  Halifax: {
+    lat: 44.65,
+    lon: -63.59,
+  },
+};
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000");
+  console.log("Listening on port 3000");
 });
+app.post("/api/weather", async (request, response) => {
+  const city = request.body.city;
+  const lat = json[city].lat;
+  const lon = json[city].lon;
+  const units = "metric";
+  const apiKey = "07c3eea7707b187a1928d807dbbf052a";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
 
-app.post("https://api.openweathermap.org/data/3.0/onecall?lat={43.6548}&lon={79.3884}&exclude=current&appid={8d4bf9966334542ae21243ea9ad16876}", (request, response) => {
-    const city = request.body.city;
-    const weather = json[city]; 
-    console.log(weather)
-    response.json(weather)
-}   
-)
+  const res = await fetch(url, { method: "GET" });
+  const weatherData = await res.json();
+  console.log(weatherData.name);
+  const temp = weatherData.main.temp + "°c";
+  const windSpeed = weatherData.wind.speed + "Km/hr";
+  const description = weatherData.weather[0].description;
+  response.json({ temp: temp, windSpeed: windSpeed, description: description });
+});
--- Assignments/Assignment2/Eric's Weather/FrontEnd/weather.js
+++ Assignments/Assignment2/Eric's Weather/FrontEnd/weather.js
@@ -1,38 +1,36 @@
- 
-const weatherURL = "http://localhost:3000/api/weather"
+const weatherURL = "http://localhost:3000/api/weather";
 
- document.addEventListener('DOMContentLoaded', async() => {
-    const select = document.getElementById('citySelect');
-    const display = document.getElementById('cityDisplay');
-    select.value = "";
-    let report = document.querySelector("#report");
-    console.log(report)
-    select.addEventListener('change', async() => {
+document.addEventListener("DOMContentLoaded", async () => {
+  const select = document.getElementById("citySelect");
+  const display = document.getElementById("cityDisplay");
+  select.value = "";
+  let report = document.querySelector("#report");
 
-        report.textContent = "";
-        if (select.value === "") {
-            display.textContent = "No city selected.";
-        } else {
-            const selectedText = select.options[select.selectedIndex].text;
-            display.textContent = "You selected: " + selectedText;
+  select.addEventListener("change", async () => {
+    report.textContent = "";
 
-            let weather = await fetch(weatherURL, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({"city": selectedText})
-                })
-                let response = await weather.json();
-                console.log(response);
-                displayReport(response);
-                    }
-    });
+    if (select.value === "") {
+      display.textContent = "No city selected.";
+    } else {
+      const selectedText = select.options[select.selectedIndex].text;
+      display.textContent = "You selected: " + selectedText;
+
+      let weather = await fetch(weatherURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ city: selectedText }),
+      });
+      let response = await weather.json();
+      displayReport(response);
+    }
+  });
 });
 
 const displayReport = (response) => {
-    for (key in response) {
-        const element = document.createElement("h2");
-        const text = `${key} : ${response[key]}`
-        element.append(text);
-        report.append(element);
-    }
-}
+  for (key in response) {
+    const element = document.createElement("h2");
+    const text = `${key} : ${response[key]}`;
+    element.append(text);
+    report.append(element);
+  }
+};
