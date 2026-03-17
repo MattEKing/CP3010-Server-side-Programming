25a35cbbdd6c997875f4a3a198c2db3c2a46c8c1
Matthew King <146943749+MattEKing@users.noreply.github.com>
2026-03-17 13:57:02 -0230

    commit

--- Assignments/Assignment4/trivia-assignment/src/app/characters/[id]/page.jsx
+++ Assignments/Assignment4/trivia-assignment/src/app/characters/[id]/page.jsx
@@ -4,7 +4,7 @@ import CharacterDetails from "../../components/characterDetails."
 export async function generateStaticParams() {
     return [{id: "1"}]
 }
-
+//commint
 export default async function({params}) {
 
     const { id } = await params
